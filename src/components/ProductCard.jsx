b467d795e28ddf3113403a0b4423701ad8bd6484
SuperPreciosa <superpreciosatienda@gmail.com>
2026-01-07 18:33:29 -0400

    Eliminar sombra oscura de las fotos del producto

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -41,7 +41,6 @@ export function ProductCard({ product, onAddToCart }) {
           alt={product.name}
           className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
       </div>
 
       <div className="p-5 flex flex-col flex-1">
